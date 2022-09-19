eea7770ceec824e6fb1620b2b6da31d39f841c1f
fitrohiza <fitrohiza@gmail.com>
2022-09-19 12:33:05 +0700

    add progress bar

--- lib/Layout/Game/Hasil.dart
+++ lib/Layout/Game/Hasil.dart
@@ -1,8 +1,8 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:percent_indicator/circular_percent_indicator.dart';
 import 'package:smartscout/Constant/DataSoalKuis.dart';
 import 'package:smartscout/Layout/Homepage.dart';
-
 import '../../Constant/Ukuran.dart';
 import '../../Constant/Warna.dart';
 
@@ -18,20 +18,18 @@ class _HasilState extends State<Hasil> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        centerTitle: true,
-        elevation: 0.0,
-        backgroundColor: warnaUngu,
-        automaticallyImplyLeading: false
-      ),
-      body:
-      Container(
+          centerTitle: true,
+          elevation: 0.0,
+          backgroundColor: warnaUngu,
+          automaticallyImplyLeading: false),
+      body: Container(
           width: MediaQuery.of(context).size.width,
           height: MediaQuery.of(context).size.height,
           decoration: const BoxDecoration(
             gradient: LinearGradient(
               begin: Alignment.topCenter,
               end: Alignment.bottomCenter,
-              colors: [ warnaUngu, warnaPurple700],
+              colors: [warnaUngu, warnaPurple700],
             ),
           ),
           child: Padding(
@@ -44,23 +42,72 @@ class _HasilState extends State<Hasil> {
                     borderRadius: BorderRadius.circular(30),
                   ),
                   child: Padding(
-                      padding: EdgeInsets.all(10.0),
+                      padding: EdgeInsets.only(
+                          top: 30, bottom: 10, left: 10, right: 10),
                       child: Container(
                         width: MediaQuery.of(context).size.width,
                         height: 0.5 * MediaQuery.of(context).size.height,
                         child: Column(
                           children: [
-                            Text(hasilJawabKuis.toString(), style: TextStyle(color: warnaHitam, fontSize: ukFormTulisanKecil)),
+                            Text("Hasil Test\n",
+                                textAlign: TextAlign.start,
+                                style: TextStyle(
+                                  color: warnaHitam,
+                                  fontSize: ukFormTulisanKecil,
+                                )),
+                            CircularPercentIndicator(
+                              radius: 100.0,
+                              lineWidth: 12.0,
+                              backgroundColor: Colors.grey,
+                              percent: hasilJawabKuis / 100,
+                              progressColor: warnaUngu,
+                              animation: true,
+                              animationDuration: 1500,
+                              center: Text(
+                                hasilJawabKuis.toString(),
+                                style: TextStyle(
+                                    color: warnaHitam,
+                                    fontSize: ukFormTulisanKecil),
+                              ),
+                            ),
+                            Container(
+                              padding: EdgeInsets.only(top: 20.0),
+                              child: Column(
+                                children: [
+                                  Text(
+                                    "Analisis Kuis",
+                                    style: TextStyle(
+                                        color: warnaHitam,
+                                        fontSize: ukFormTulisanKecil),
+                                  ),
+                                  Row(
+                                    children: [
+                                      Text(
+                                        "Benar",
+                                        style: TextStyle(
+                                            color: warnaHitam,
+                                            fontSize: ukIsiTulisanKecil),
+                                      ),
+                                      Text(
+                                        "Salah",
+                                        style: TextStyle(
+                                            color: warnaHitam,
+                                            fontSize: ukIsiTulisanKecil),
+                                      ),
+                                    ],
+                                  )
+                                ],
+                              ),
+                            )
                           ],
                         ),
-                      )
-                  ),
+                      )),
                   elevation: 4,
                   shadowColor: warnaHitam,
                 ),
                 const SizedBox(height: 10),
                 InkWell(
-                  onTap: (){
+                  onTap: () {
                     keHomepage();
                   },
                   child: Container(
@@ -70,15 +117,15 @@ class _HasilState extends State<Hasil> {
                           shape: BoxShape.rectangle,
                           borderRadius: BorderRadius.circular(30),
                           border: Border.all(width: 2, color: warnaPutih),
-                          color: warnaPurple700
-                      ),
+                          color: warnaPurple700),
                       child: Row(
                         crossAxisAlignment: CrossAxisAlignment.center,
                         mainAxisAlignment: MainAxisAlignment.center,
                         children: [
                           const Icon(Icons.home, color: warnaPutih),
                           const SizedBox(width: 10),
-                          Text("Beranda",
+                          Text(
+                            "Beranda",
                             textAlign: TextAlign.start,
                             style: TextStyle(
                                 letterSpacing: 2.0,
@@ -87,20 +134,18 @@ class _HasilState extends State<Hasil> {
                                 fontSize: ukFormTulisanKecil),
                           ),
                         ],
-                      )
-                  ),
+                      )),
                 )
               ],
             ),
-          )
-      ),
+          )),
     );
   }
 
-  void keHomepage(){
+  void keHomepage() {
     Navigator.pushAndRemoveUntil(
         context,
         MaterialPageRoute(builder: (context) => const Homepage()),
-            (route) => false);
+        (route) => false);
   }
 }
