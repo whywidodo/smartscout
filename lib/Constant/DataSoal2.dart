4ef080d6e14f1c209f37f21d60809a2dec465d12
fitrohiza <fitrohiza@gmail.com>
2022-09-25 19:36:17 +0700

    add soal baru

---
+++ lib/Constant/DataSoal2.dart
@@ -0,0 +1,136 @@
+int valueSoal1 = 0;
+int valueSoal2 = 0;
+int valueSoal3 = 0;
+int valueSoal4 = 0;
+int valueSoal5 = 0;
+int valueSoal6 = 0;
+int valueSoal7 = 0;
+int valueSoal8 = 0;
+int valueSoal9 = 0;
+int valueSoal10 = 0;
+
+int jawab1 = 0;
+int jawab2 = 0;
+int jawab3 = 0;
+int jawab4 = 0;
+int jawab5 = 0;
+int jawab6 = 0;
+int jawab7 = 0;
+int jawab8 = 0;
+int jawab9 = 0;
+int jawab10 = 0;
+
+int hasilJawabKuis = 0;
+double hasilBenar = 0;
+double hasilSalah = 0;
+
+var nextSoal1 = "NextSoal1";
+var nextSoal2 = "NextSoal2";
+var nextSoal3 = "NextSoal3";
+var nextSoal4 = "NextSoal4";
+var nextSoal5 = "NextSoal5";
+var nextSoal6 = "NextSoal6";
+var nextSoal7 = "NextSoal7";
+var nextSoal8 = "NextSoal8";
+var nextSoal9 = "NextSoal9";
+var nextSoal10 = "NextSoal10";
+var nextFinishKU = "NextFinishKuisUmum";
+
+var prevSoal1 = "PrevSoal1";
+var prevSoal2 = "PrevSoal2";
+var prevSoal3 = "PrevSoal3";
+var prevSoal4 = "PrevSoal4";
+var prevSoal5 = "PrevSoal5";
+var prevSoal6 = "PrevSoal6";
+var prevSoal7 = "PrevSoal7";
+var prevSoal8 = "PrevSoal8";
+var prevSoal9 = "PrevSoal9";
+var prevSoal10 = "PrevSoal10";
+
+var nomorSoalKuis1 = "Soal 1";
+var soalKuis1 = "Apa nama cabang pramuka milik belanda sebelum namanya diubah "
+    "menjadi Nederlans Indische Padvinders Vereniging atau NIVP?";
+var kluKuis1 = "-";
+var jawabanKuis1a = "Javananche Padvinders Organisatie";
+var jawabanKuis1b = "Nederlandeshe Padvinders Organisatie";
+var jawabanKuis1c = "Indonesische Padvinderij Organisatie";
+var jawabanKuis1d = "Jong Indonesische Padvinderij Organisatie";
+
+var nomorSoalKuis2 = "Soal 2";
+var soalKuis2 = "Pada tanggal 23 Mei 1928 muncul organisasi PAPI atau Persaudaraan"
+    "Antar Pandu, yang digunakan sebagai wadah untuk para pandu Di Indonesia, berikut ini "
+    "merupakan organisasi yang bergabung menjadi anggota dari PAPI, kecuali?";
+var kluKuis2 = "-";
+var jawabanKuis2a = "INPO";
+var jawabanKuis2b = "SIAP";
+var jawabanKuis2c = "NPO";
+var jawabanKuis2d = "PSS";
+
+var nomorSoalKuis3 = "Soal 3";
+var soalKuis3 = "Pada Undang-undang dan pasal ke berapakah Kode Kehormatan diatur?";
+var kluKuis3 = "-";
+var jawabanKuis3a = "UU No. 12 Tahun 2010 pasal 6";
+var jawabanKuis3b = "UU No. 12 Tahun 2010 pasal 10";
+var jawabanKuis3c = "UU No. 12 Tahun 2011 pasal 6";
+var jawabanKuis3d = "UU No. 12 Tahun 2011 pasal 10";
+
+var nomorSoalKuis4 = "Soal 4";
+var soalKuis4 = "Berikut ini bunyi Dasadarma ke-5 yang benar adalah?";
+var kluKuis4 = "-";
+var jawabanKuis4a = "Takwa kepada Tuhan YME";
+var jawabanKuis4b = "Rela Menolong dan Tabah";
+var jawabanKuis4c = "Disiplin berani dan setia";
+var jawabanKuis4d = "Rajin trampil dan gembira";
+
+var nomorSoalKuis5 = "Soal 5";
+var soalKuis5 = "Pada golongan prauka apakah yang memiliki kode kehormatan berupa "
+    "Dwisatya dan Dwidarma?";
+var kluKuis5 = "-";
+var jawabanKuis5a = "Siaga, Madya, Rakit";
+var jawabanKuis5b = "Siaga, Penggalang, Pembina";
+var jawabanKuis5c = "Siaga, Penggalang, Penegak";
+var jawabanKuis5d = "Siaga, Ramu, Rakit";
+
+var nomorSoalKuis6 = "Soal 6";
+var soalKuis6 ="Pada tahun berapakah pendiri lambang pramuka Indonesia di lahirkan";
+var kluKuis6 = "-";
+var jawabanKuis6a = "29 Mei 1979";
+var jawabanKuis6b = "29 Februari 1979";
+var jawabanKuis6c = "29 Mei 1967";
+var jawabanKuis6d = "29 Februari 1967";
+
+var nomorSoalKuis7 = "Soal 7";
+var soalKuis7 = "Berapa jumlah Dwidarma?";
+var kluKuis7 = "-";
+var jawabanKuis7a = "Sepuluh";
+var jawabanKuis7b = "Tiga";
+var jawabanKuis7c = "Dua";
+var jawabanKuis7d = "Sembilan";
+
+var nomorSoalKuis8 = "Soal 8";
+var soalKuis8 = "Lambang ini mengkiaskan bahwa setiap Pramuka adalah manusia yang berguna dan membaktikan"
+    " diri dan kegunaanya kepada kepentingan Tanah Air, Bangsa dan Negara Kesatuan Republik Indonesia serta"
+    " kepada umat manusia, lambang apakah ini?";
+var kluKuis8 = "-";
+var jawabanKuis8a = "Buah Jambu";
+var jawabanKuis8b = "Buah Mangga";
+var jawabanKuis8c = "Buah Kurma";
+var jawabanKuis8d = "Buah Nyiur";
+
+var nomorSoalKuis9 = "Soal 9";
+var soalKuis9 = "Pada tahun berapakah Baden Powell atau Bapak Pandu menerima"
+    " Gelar Kehormatan dan Universitas Cambridge?";
+var kluKuis9 = "-";
+var jawabanKuis9a = "1923";
+var jawabanKuis9b = "1931";
+var jawabanKuis9c = "1929";
+var jawabanKuis9d = "1910";
+
+var nomorSoalKuis10 = "Soal 10";
+var soalKuis10 = "Julukan apa yang diberikan kepada Baden Powell waktu berseolah"
+    " di Charterhouse School?";
+var kluKuis10 = "-";
+var jawabanKuis10a = "Impeesa";
+var jawabanKuis10b = "Baron";
+var jawabanKuis10c = "Bee-Pee";
+var jawabanKuis10d = "Bathing-Towel";
